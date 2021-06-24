28f3b491b7d856986be88447781d1a5b4baa4772
0x04 <ok@0x04.de>
2021-06-24 06:57:15 +0200

    feat(checkbox): story adjustments

--- atomic-design/atoms/checkbox/stories/checkbox.stories.tsx
+++ atomic-design/atoms/checkbox/stories/checkbox.stories.tsx
@@ -2,36 +2,40 @@ import { Checkbox, CheckboxProps } from "@dekk-ui/checkbox";
 import { Story } from "@storybook/react";
 import React from "react";
 
-const Template: Story<CheckboxProps> = args => {
+export const Simple: Story<CheckboxProps> = args => {
 	return (
-		<>
-			<div>
-				<label>
-					<Checkbox {...args} />
-				</label>
-			</div>
-			<div>
-				<label>
-					<Checkbox {...args} style={{ width: "64px", height: "64px" }} />
-				</label>
-			</div>
-			<div>
-				<label>
-					<Checkbox {...args} style={{ width: "64px", height: "32px" }} />
-				</label>
-			</div>
-			<div>
-				<label>
-					<Checkbox {...args} style={{ width: "32px", height: "64px" }} />
-				</label>
-			</div>
-		</>
+		<label>
+			<Checkbox {...args} />
+		</label>
 	);
 };
 
-export const Simple = Template.bind({});
-
 Simple.args = {};
+
+export const Scaling: Story<CheckboxProps> = args => {
+	return (
+		<div
+			style={{
+				display: "grid",
+				gridGap: "1rem",
+			}}
+		>
+			<label>
+				<Checkbox {...args} style={{ width: "64px", height: "64px" }} />
+			</label>
+
+			<label>
+				<Checkbox {...args} style={{ width: "64px", height: "32px" }} />
+			</label>
+
+			<label>
+				<Checkbox {...args} style={{ width: "32px", height: "64px" }} />
+			</label>
+		</div>
+	);
+};
+
+Scaling.args = {};
 
 const story = {
 	component: Checkbox,
