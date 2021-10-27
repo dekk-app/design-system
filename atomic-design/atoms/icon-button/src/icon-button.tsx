a75572d9fe82ae19b08545bcc3ad390a8e222578
Gregor Adams <greg@pixelass.com>
2021-10-28 00:57:51 +0200

    feat(icon-button): adjust to new icons api

--- atomic-design/atoms/icon-button/src/icon-button.tsx
+++ atomic-design/atoms/icon-button/src/icon-button.tsx
@@ -3,11 +3,11 @@ import React, { FC } from "react";
 import { StyledButton, StyledPath, StyledSvg } from "./styled";
 import { IconButtonProps } from "./types";
 
-export const IconButton: FC<IconButtonProps> = ({ icon, size, ...props }) => {
+export const IconButton: FC<IconButtonProps> = ({ icon, ...props }) => {
 	return (
 		<StyledButton {...props} aria-label={icon}>
-			<StyledSvg height={size} width={size} viewBox={`0 0 ${size} ${size}`}>
-				<StyledPath d={icons[size][icon]} />
+			<StyledSvg height={24} width={24} viewBox={`0 0 24 24`}>
+				<StyledPath d={icons[icon]} />
 			</StyledSvg>
 		</StyledButton>
 	);
