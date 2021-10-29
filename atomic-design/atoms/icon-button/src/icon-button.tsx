3cefc41faaaccf81fec107525b615cb122ed7264
Gregor Adams <greg@pixelass.com>
2021-10-29 11:24:19 +0200

    feat(icon-button): use forwardProp

--- atomic-design/atoms/icon-button/src/icon-button.tsx
+++ atomic-design/atoms/icon-button/src/icon-button.tsx
@@ -1,14 +1,16 @@
 import { icons } from "@dekk-ui/icons";
-import React, { FC } from "react";
+import React, { forwardRef } from "react";
 import { StyledButton, StyledPath, StyledSvg } from "./styled";
 import { IconButtonProps } from "./types";
 
-export const IconButton: FC<IconButtonProps> = ({ icon, ...props }) => {
-	return (
-		<StyledButton {...props} aria-label={icon}>
-			<StyledSvg height={24} width={24} viewBox="0 0 24 24">
-				<StyledPath d={icons[icon]} />
-			</StyledSvg>
-		</StyledButton>
-	);
-};
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+	({ icon, ...props }, ref) => {
+		return (
+			<StyledButton ref={ref} {...props} aria-label={icon}>
+				<StyledSvg height={24} width={24} viewBox="0 0 24 24">
+					<StyledPath d={icons[icon]} />
+				</StyledSvg>
+			</StyledButton>
+		);
+	}
+);
